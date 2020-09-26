35ade31205e0b3342330aa471a7769ad9fb44f2f
BAC <41997195+bac0id@users.noreply.github.com>
2020-09-26 22:47:22 +0800

    Create RandomExtension.cs

---
+++ RandomExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class RandomExtension
+{
+	/// <summary>
+	///		Returns a random number within a specified range.
+	/// </summary>
+	/// <param name="random"></param>
+	/// <param name="minValue">
+	///		The inclusive lower bound of the random number returned.
+	/// </param>
+	/// <param name="maxValue">
+	///		The exclusive upper bound of the random number returned.
+	///		<paramref name="maxValue"/> must be greater than or equal to minValue.
+	///	</param>
+	/// <param name="digit">
+	///		The number of fractional digits in the return value.
+	///	</param>
+	/// <returns>
+	///		A double-precision floating point number greater than or equal to <paramref name="minValue"/>, and less than <paramref name="maxValue"/>.
+	/// </returns>
+	public static double NextDouble(this Random random, double minValue, double maxValue, int digit) {
+		return Math.Round(random.NextDouble() * (maxValue - minValue) + minValue, digit);
+	}
+}
